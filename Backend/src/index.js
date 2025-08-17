a2762ac79fad9075a415b1bbedcf72ab7d48f7eb
OmYadav3 <omydv3@gmail.com>
2025-08-17 12:01:04 +0530

    mongodb connection complete

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,10 +1,13 @@
 import dotenv from "dotenv"
 import {app} from "./app.js"
+import connectionDb from "./db/db.js"
 
 dotenv.config({
    path:'./.env'
 })
 
+
+connectionDb();
 
 app.listen(process.env.PORT || 8000, () => {
       console.log(`Server is running at port ${process.env.PORT}`);
