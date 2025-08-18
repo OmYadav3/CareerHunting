10bb19d86d0ae2393a17e8814824288753de9328
OmYadav3 <omydv3@gmail.com>
2025-08-18 10:09:59 +0530

    add the login contoller

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -40,3 +40,69 @@ export const register = async (req, res) => {
    }
 };
 
+
+export const login = async () => {
+   try {
+      const { fullname, email, password } = res.body;
+
+      if (!fullname || !email || password || !role) {
+         return res.status(400).json({
+            message: "Something is missing in the required field ",
+            success: false,
+         });
+      }
+
+      let user = await User.findOne({ email });
+      if (!user) {
+         return res.status(400).json({
+            message: "user are not exists with this email",
+            success: false,
+         });
+      }
+
+      const isPasswordMatch = await bcrypt.compare(password, user.password);
+      if (!isPasswordMatch) {
+         return res.status(400).json({
+            message: "password is incorrect",
+            success: false,
+         });
+      }
+
+      if ((role = !user.role)) {
+         return res.status(400).json({
+            message: "Account do not exists with this role",
+            success: false,
+         });
+      }
+
+      const tokenData = {
+         userId: user._id,
+      };
+      const token = await jwt.sign(tokenData, process.env.SECRET_TOKEN_KEY, {
+         expiresin: "1d",
+      });
+
+      user = {
+        _id: user._id,
+        fullname:user.fullname,
+        email:user.email,
+        phoneNumber: user.phoneNumber,
+        role:user.role,
+        profile: user.profile
+      }
+
+      return res
+         .status(200)
+         .cookie("token", token, {
+            maxAge: 1 * 24 * 60 * 60 * 1000,
+            httpOnly: true,
+            sameSite: 'strict',
+         })
+         .json({
+            message: `Welcome back! ${user.fullname}`,
+            success: true,
+         });
+   } catch (error) {
+    console.log(error, 'Something error in login the user')
+   }
+};
