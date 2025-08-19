83fb0f2296d3001c3e073c320f703478144eed25
OmYadav3 <omydv3@gmail.com>
2025-08-19 13:45:01 +0530

    add decelaration the routes of user

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,6 +14,13 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+/* ===========ROUTES IMPORTS============ */
+import userRouter from './routes/user.route.js'
 
+
+
+
+/* ============ROUTES DECELARATIONS============== */
+app.use('/api/v1/users', userRouter)
 
 export { app }
