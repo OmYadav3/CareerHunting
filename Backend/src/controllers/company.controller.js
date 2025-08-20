8c2cb6e7798b81726d0d175de861ff0c7114d3e2
OmYadav3 <omydv3@gmail.com>
2025-08-20 23:07:22 +0530

    add the getCompanyById controller

--- Backend/src/controllers/company.controller.js
+++ Backend/src/controllers/company.controller.js
@@ -41,18 +41,57 @@ export const registerCompany = async(req, res) => {
 
 export const getCompany = async (req, res) => {
     try {
-        
+
         const userId = req.id
         const companies = await Company.find({userId})
         if (!companies) {
-            return res.status(400).json({
+            return res.status(404).json({
                 message:"Cannot find any company",
                 success:false
             })
         }
         
+        return res.status(200).json({
+            message:" Found all Companies by registered the user",
+            companies,
+            success: true
+        })
+
     } catch (error) {
         console.log("ERROR TO GETING THE COMPANY: ", error)
+        return res.status(500).json({
+         message: "Internal server error",
+         success: false,
+      });
+    }
+}
+
+export const getCompanyById = async (req, res) => {
+    try {
+        
+        const companyId = req.params.id
+        if (!companyId) {
+            return res.status(400).json({
+                message:"Cannot found the companyId",
+                success:false
+            })
+        }
+
+        const company = await Company.findById({companyId})
+        if (company) {
+            return res.status(404).json({
+                message:"Cannot found the Company by Id "
+            })
+        }
+
+        return res.status(200).json({
+            message: "Companyu found Successfully by Id ",
+            company,
+            success:true
+        })
+
+    } catch (error) {
+         console.log("ERROR TO GETING THE COMPANY BY ID: ", error)
         return res.status(500).json({
          message: "Internal server error",
          success: false,
