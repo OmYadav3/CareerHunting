b1aa85ed511be9656b43bb96439f3aa3f5c43c7f
OmYadav3 <omydv3@gmail.com>
2025-08-21 23:02:59 +0530

    add the updateCompany controller

--- Backend/src/controllers/company.controller.js
+++ Backend/src/controllers/company.controller.js
@@ -1,100 +1,127 @@
-import Company from '../models/company.model.js'
+import Company from "../models/company.model.js";
 
-export const registerCompany = async(req, res) => {
-    try {
-        const {companyName} = req.body
-        if (!companyName) {
-            return res.status(400).json({
-                message:"Company name is already exists",
-                success:false
-            })
-        }
+export const registerCompany = async (req, res) => {
+   try {
+      const { companyName } = req.body;
+      if (!companyName) {
+         return res.status(400).json({
+            message: "Company name is already exists",
+            success: false,
+         });
+      }
 
-        let company = await Company.findOne({companyName})
-        if (!company) {
-            return res.status(400).json({
-                message:"You cannot register the same company",
-                success:false
-            })
-        }
+      let company = await Company.findOne({ companyName });
+      if (!company) {
+         return res.status(400).json({
+            message: "You cannot register the same company",
+            success: false,
+         });
+      }
 
-        company = await Company.create({
-            name:companyName,
-            userId:req.id
-        })
+      company = await Company.create({
+         name: companyName,
+         userId: req.id,
+      });
 
-        return res.status(201).json({
-            message: "Company register Successfully",
-            company,
-            success:true
-        })
-
-    } catch (error) {
-        console.log("ERROR TO REGISTER: ", error)
-        return res.status(500).json({
+      return res.status(201).json({
+         message: "Company register Successfully",
+         company,
+         success: true,
+      });
+   } catch (error) {
+      console.log("ERROR TO REGISTER: ", error);
+      return res.status(500).json({
          message: "Internal server error",
          success: false,
       });
-
-    }
-}
+   }
+};
 
 export const getCompany = async (req, res) => {
-    try {
+   try {
+      const userId = req.id;
+      const companies = await Company.find({ userId });
+      if (!companies) {
+         return res.status(404).json({
+            message: "Cannot find any company",
+            success: false,
+         });
+      }
 
-        const userId = req.id
-        const companies = await Company.find({userId})
-        if (!companies) {
-            return res.status(404).json({
-                message:"Cannot find any company",
-                success:false
-            })
-        }
-        
-        return res.status(200).json({
-            message:" Found all Companies by registered the user",
-            companies,
-            success: true
-        })
-
-    } catch (error) {
-        console.log("ERROR TO GETING THE COMPANY: ", error)
-        return res.status(500).json({
+      return res.status(200).json({
+         message: " Found all Companies by registered the user",
+         companies,
+         success: true,
+      });
+   } catch (error) {
+      console.log("ERROR TO GETING THE COMPANY: ", error);
+      return res.status(500).json({
          message: "Internal server error",
          success: false,
       });
-    }
-}
+   }
+};
 
 export const getCompanyById = async (req, res) => {
-    try {
-        
-        const companyId = req.params.id
-        if (!companyId) {
-            return res.status(400).json({
-                message:"Cannot found the companyId",
-                success:false
-            })
-        }
+   try {
+      const companyId = req.params.id;
+      if (!companyId) {
+         return res.status(400).json({
+            message: "Cannot found the companyId",
+            success: false,
+         });
+      }
 
-        const company = await Company.findById({companyId})
-        if (company) {
-            return res.status(404).json({
-                message:"Cannot found the Company by Id "
-            })
-        }
+      const company = await Company.findById({ companyId });
+      if (company) {
+         return res.status(404).json({
+            message: "Cannot found the Company by Id ",
+         });
+      }
 
-        return res.status(200).json({
-            message: "Companyu found Successfully by Id ",
-            company,
-            success:true
-        })
-
-    } catch (error) {
-         console.log("ERROR TO GETING THE COMPANY BY ID: ", error)
-        return res.status(500).json({
+      return res.status(200).json({
+         message: "Companyu found Successfully by Id ",
+         company,
+         success: true,
+      });
+   } catch (error) {
+      console.log("ERROR TO GETING THE COMPANY BY ID: ", error);
+      return res.status(500).json({
          message: "Internal server error",
          success: false,
       });
-    }
-}
+   }
+};
+
+export const updateCompany = async (req, res) => {
+   try {
+      const { name, description, website, location } = req.body;
+      const file = req.file;
+      /* =============CLOUDNARY WORK HERE ============== */
+
+      const updateData = { name, description, website, location };
+      const company = await Company.findByIdAndUpdate(
+         req.params.id,
+         updateCompany,
+         { new: true }
+      );
+      if (company) {
+         return res.status(404).json({
+            message: "Company not found",
+            success: false,
+         });
+      }
+
+      return res.status(200).json({
+         message: "Update the Company information successfully",
+         company,
+         success: true,
+      });
+   } catch (error) {
+      console.log("ERROR TO UPDATING THE COMPANY: ", error);
+      return res.status(500).json({
+         message: "Internal server error",
+         success: false,
+      });
+   }
+};
