287181c9068d2d1d4a14f651ac26dfa17e48b210
OmYadav3 <omydv3@gmail.com>
2025-08-22 23:45:12 +0530

    test the updateCompany endpoint

--- Backend/src/controllers/company.controller.js
+++ Backend/src/controllers/company.controller.js
@@ -106,16 +106,19 @@ export const getCompanyById = async (req, res) => {
 export const updateCompany = async (req, res) => {
    try {
       const { name, description, website, location } = req.body;
+      // console.log(req.body)
       const file = req.file;
+
       /* =============CLOUDNARY WORK HERE ============== */
 
       const updateData = { name, description, website, location };
+
       const company = await Company.findByIdAndUpdate(
          req.params.id,
          updateData,
          { new: true }
       );
-      if (company) {
+      if (!company) {
          return res.status(404).json({
             message: "Company not found",
             success: false,
