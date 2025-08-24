76929f6316281ed8f9cbbea687a58ab0c841b108
OmYadav3 <omydv3@gmail.com>
2025-08-24 22:31:15 +0530

    add the application declaration

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,30 +1,31 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
 /* =========== MIDDLEWARES ============== */
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
-app.use(express.json())
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(
+   cors({
+      origin: process.env.CORS_ORIGIN,
+      credentials: true,
+   })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 /* ===========ROUTES IMPORTS============ */
-import userRoute from './routes/user.route.js'
-import companyRoute from './routes/company.route.js'
-import jobRoute from './routes/job.route.js'
-
-
-
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
 
 /* ============ROUTES DECLARATIONS============== */
-app.use('/api/v1/user', userRoute)
-app.use('/api/v1/company', companyRoute)
-app.use('/api/v1/job', jobRoute)
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
-export { app }
+export { app };
