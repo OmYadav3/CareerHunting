5484fa497a119a0318af1dd2d2ab27c8622dc290
OmYadav3 <omydv3@gmail.com>
2025-08-28 23:13:28 +0530

    update the Navbar component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Navbar from './components/shared/Navbar'
 
 function App() {
 
   return (
     <>
-    <h1>CareerHunting</h1>
+    {/* <h1>CareerHunting</h1> */}
+    <Navbar/>
     </>
   )
 }
