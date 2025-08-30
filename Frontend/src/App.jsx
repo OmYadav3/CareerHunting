03325f18d36c47ae745820dd726b19eeca0472e4
OmYadav3 <omydv3@gmail.com>
2025-08-30 22:41:53 +0530

    update the app.jsx, home.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
-import Login from './components/auth/Login';
-import Signup from './components/auth/Signup';
-import Home from './components/Home';
+import Login from './components/auth/Login.jsx';
+import Signup from './components/auth/Signup.jsx';
+import Home from './components/Home.jsx';
 
 const appRouter = createBrowserRouter([
 
