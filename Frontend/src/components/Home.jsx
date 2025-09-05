34cd779c1c7d2bbe2c3b85c2d0a55ecc1719702c
OmYadav3 <omydv3@gmail.com>
2025-09-05 18:33:36 +0530

    add the heroSection

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './shared/Navbar.jsx'
+import HeroSection from './HeroSection.jsx'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
+      <HeroSection/>
     </div>
   )
 }
