6b5876d35c4eb75fb6e77f06c70c1753ae34a0e4
OmYadav3 <omydv3@gmail.com>
2025-09-05 18:14:53 +0530

    add the authSlice file

---
+++ Frontend/src/redux/authSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authslice = createSlice({
+   name:"auth",
+   initialState:{
+      loading:false
+   },
+   reducers:{
+      // actions 
+      setloading:(state, action) => {
+         state.loading = action.payload;
+      }
+   }
+
+})
+
+export const { setloading } = authslice.actions;
+export default authslice.reducer; 
