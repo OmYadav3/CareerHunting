c77d334625948aa491dda33f0b9b934ec723846e
OmYadav3 <omydv3@gmail.com>
2025-09-23 21:54:45 +0530

    update controller  of job and applications

--- Backend/src/controllers/job.controller.js
+++ Backend/src/controllers/job.controller.js
@@ -106,38 +106,58 @@ export const getAllJobs = async (req, res) => {
 };
 
 /* =======FOR STUDENTS =========== */
+// export const getJobById = async (req, res) => {
+//    try {
+//       const jobId = req.params.id;
+//       if (!jobId) {
+//          return res.status(404).json({
+//             message: "Cannot found the JobId",
+//             success: false,
+//          });
+//       }
+
+//       const job = await Job.findById(jobId).populate({
+//          path:"applications"
+//       });
+
+//       if (!job) {
+//          return res.status(404).json({
+//             message: "Cannot found the Job by Id",
+//             success: false,
+//          });
+//       }
+
+//       return res.status(200).json({
+//          job,
+//          success: true,
+//       });
+//    } catch (error) {
+//       console.log("ERROR TO GETING THE JOB BY ID: ", error);
+//       return res.status(500).json({
+//          message: "Internal server error",
+//          success: false,
+//       });
+//    }
+// };
+
 export const getJobById = async (req, res) => {
-   try {
-      const jobId = req.params.id;
-      if (!jobId) {
-         return res.status(404).json({
-            message: "Cannot found the JobId",
-            success: false,
-         });
-      }
+    try {
+        const jobId = req.params.id;
+        const job = await Job.findById(jobId).populate({
+            path:"applications"
+        });
+        if (!job) {
+            return res.status(404).json({
+                message: "Jobs not found.",
+                success: false
+            })
+        };
+        return res.status(200).json({ job, success: true });
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-      const job = await Job.findById(jobId).populate({
-         path:"applications"
-      });
-      if (!job) {
-         return res.status(404).json({
-            message: "Cannot found the Job by Id",
-            success: false,
-         });
-      }
-
-      return res.status(200).json({
-         job,
-         success: true,
-      });
-   } catch (error) {
-      console.log("ERROR TO GETING THE JOB BY ID: ", error);
-      return res.status(500).json({
-         message: "Internal server error",
-         success: false,
-      });
-   }
-};
 
 /* =========FOR ADMIN ============ */
 export const getAdminJobs = async (req, res) => {
