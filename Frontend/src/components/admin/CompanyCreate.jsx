660284fae7eb8e591816dd50400866db027bf02e
Om Yadav <omydv3@gmail.com>
2025-10-02 12:23:50 +0530

    fixed the UI of CompanyCreate page

--- Frontend/src/components/admin/CompanyCreate.jsx
+++ Frontend/src/components/admin/CompanyCreate.jsx
@@ -1,24 +1,38 @@
-import React from 'react'
-import Navbar from '../shared/Navbar'
-import { Label } from '../ui/label'
-import { Input } from '../ui/input'
+import React from "react";
+import Navbar from "../shared/Navbar";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { useNavigate } from "react-router-dom";
 
 const CompanyCreate = () => {
-  return (
-    <div>
-      <Navbar/>
-      <div className="max-w-4xl mx-auto">
-         <h1 className='font-bold text-2xl'>Your Company Name</h1>
-         <p>What would you like to give your company name? You need to change this later.</p>
-         <Label>Company Name </Label>
-         <Input
-         type='text'
-         className='my-2'
-         placeholder='JobHunt, Micrsoft, etc.'
-         />
-      </div>
-    </div>
-  )
-}
 
-export default CompanyCreate
+  const navigate = useNavigate( )
+   
+    return (
+        <div>
+            <Navbar />
+            <div className="max-w-4xl mx-auto">
+                <div className="my-10">
+                    <h1 className="font-bold text-2xl">Your Company Name</h1>
+                    <p>
+                        What would you like to give your company name? You need
+                        to change this later.
+                    </p>
+                </div>
+                <Label>Company Name </Label>
+                <Input
+                    type="text"
+                    className="my-2"
+                    placeholder="JobHunt, Micrsoft, etc."
+                />
+                <div className="flex items-center gap-2 my-10">
+                    <Button variant="outline" onClick={() => navigate("/admin/companies")} >Cancel</Button>
+                    <Button>Continue</Button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CompanyCreate;
