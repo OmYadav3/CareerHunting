2c0a6bfaa280dc2154416d56d47a0a55f32a6a28
Om Yadav <omydv3@gmail.com>
2025-11-01 21:14:38 +0530

    update the applicants controller

--- Backend/src/controllers/application.controller.js
+++ Backend/src/controllers/application.controller.js
@@ -132,37 +132,30 @@ export const getAppliedJobs = async (req, res) => {
 //    }
 // };
 
-export const getApplicants = async (req, res) => {
-   try {
-      const jobId = req.params.id;
-
-      // Find all applications for this job
-      const applications = await Application.find({ job: jobId })
-         .sort({ createdAt: -1 })
-         .populate({
-            path: "applicant", // populate applicant details
-            options: { sort: { createdAt: -1 } },
-         });
-
-      if (!applications || applications.length === 0) {
-         return res.status(404).json({
-            message: "No applicants found for this job",
-            success: false,
-         });
-      }
-
-      return res.status(200).json({
-         applicants: applications,
-         success: true,
-      });
-   } catch (error) {
-      console.log("ERROR WHILE GETTING APPLICANTS ", error);
-      return res.status(500).json({
-         message: "Internal server error",
-         success: false,
-      });
-   }
-};
+export const getApplicants = async (req,res) => {
+    try {
+        const jobId = req.params.id;
+        const job = await Job.findById(jobId).populate({
+            path:'applications',
+            options:{sort:{createdAt:-1}},
+            populate:{
+                path:'applicant'
+            }
+        });
+        if(!job){
+            return res.status(404).json({
+                message:'Job not found.',
+                success:false
+            })
+        };
+        return res.status(200).json({
+            job, 
+            succees:true
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 export const updateStatus = async (req, res) => {
    try {
