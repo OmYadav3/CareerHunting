329943ecb928992c1f0f3a1e8b69f88c6423e3a3
Om Yadav <omydv3@gmail.com>
2025-11-04 10:07:00 +0530

    ApI service implemented

--- Frontend/src/components/AppliedJobTable.jsx
+++ Frontend/src/components/AppliedJobTable.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from './ui/table'
-import { Badge } from './ui/badge'
-import { useSelector } from 'react-redux'
+import React from "react";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "./ui/table";
+import { Badge } from "./ui/badge";
+import { useSelector } from "react-redux";
 
-const allAppliedJobs =[1, 2, 3, 4]
-
+const allAppliedJobs = [1, 2, 3, 4];
 
 const AppliedJobTable = () => {
-    // const {allAppliedJobs} = useSelector(store=>store.job);
+    const {allAppliedJobs} = useSelector(store=>store.job);
     return (
         <div>
             <Table>
@@ -21,20 +28,22 @@ const AppliedJobTable = () => {
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    {
-                        allAppliedJobs.length <= 0 ? <span>You haven't applied any job yet.</span> : allAppliedJobs.map((index) => (
-                            <TableRow key={index}>
-                                <TableCell>17-09-2025</TableCell>
-                                <TableCell>Frontend Developer</TableCell>
-                                <TableCell>Google</TableCell>
-                                <TableCell className="text-right"><Badge>Selected</Badge></TableCell>
+                    {allAppliedJobs.length <= 0 ? (
+                        <span>You haven't applied any job yet.</span>
+                    ) : (
+                        allAppliedJobs.map((appliedJob) => (
+                            <TableRow key={appliedJob._id}>
+                                <TableCell>{appliedJob.createdAt.split("T")[0]}</TableCell>
+                                <TableCell>{appliedJob.job.title}</TableCell>
+                                <TableCell>{appliedJob.job.company.name}</TableCell>
+                                 <TableCell className="text-right"><Badge className={`${appliedJob?.status === "rejected" ? 'bg-red-400' : appliedJob.status === 'pending' ? 'bg-gray-400' : 'bg-green-400'}`}>{appliedJob.status.toUpperCase()}</Badge></TableCell>
                             </TableRow>
                         ))
-                    }
+                    )}
                 </TableBody>
             </Table>
         </div>
-    )
-}
+    );
+};
 
-export default AppliedJobTable
+export default AppliedJobTable;
--- Frontend/src/components/Profile.jsx
+++ Frontend/src/components/Profile.jsx
@@ -8,10 +8,12 @@ import { Contact, Mail, Pen } from "lucide-react";
 import AppliedJobTable from "./AppliedJobTable";
 import UpdateProfileDialog from "./UpdateProfileDialog";
 import { useSelector } from "react-redux";
+import useGetAppliedJobs from "../hooks/useGetAppliedJobs";
  
 const isResume = true;
 
 const Profile = () => {
+    useGetAppliedJobs();
     const [open, setOpen] = useState(false);
     const { user } = useSelector(store => store.auth)
 
